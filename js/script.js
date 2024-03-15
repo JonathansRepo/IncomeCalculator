740a7e837f34ca6265d4726b9ce51866bdc4c737
Jonathan Edwards <spiraljonathan@gmail.com>
2024-03-15 13:39:04 +0000

    JE: Calculating monthly income

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 'use strict';
 const btnCalculate = document.querySelector('.btn-calculate');
-const inputIncome = document.querySelector('.input-gross-income');
-const pensionContribution = document.querySelector(
+const inputAnnualIncome = document.querySelector('.input-gross-income');
+const pensionMonthlyContribution = document.querySelector(
   '.input-pension-contribution'
 );
-
-console.log(pensionContribution);
+const monthlyGrossIncomeLabel = document.querySelector('.monthly-gross-income');
 
 btnCalculate.addEventListener('click', function (event) {
   event.preventDefault();
-  const income = inputIncome.value;
-  console.log(income);
-  const pension = pensionContribution.value;
-  console.log(pension);
+  const annualIncome = inputAnnualIncome.value;
+  const monthlyPension = pensionMonthlyContribution.value;
+  const grossMonthlyIncome = annualIncome / 12;
+
+  monthlyGrossIncomeLabel.textContent = `Your monthly gross income is: £${grossMonthlyIncome}`;
+  const taxableIncome = grossMonthlyIncome - monthlyPension;
 });
