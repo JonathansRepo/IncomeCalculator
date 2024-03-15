a80be14bb629ab769454c4a5c4f408127ae00ea8
Jonathan Edwards <spiraljonathan@gmail.com>
2024-03-15 15:46:10 +0000

    JE: Added calculating and displaying national insurance

---
+++ js/nationalInsurance.js
@@ -0,0 +1,21 @@
+const calculateMonthlyNationalInsurance = function (grossAnnualIncome) {
+  const lowerThreshold = 12570;
+  const lowerRate = 0.1;
+  const upperThreshold = 50270;
+  const upperRate = 0.2;
+
+  if (grossAnnualIncome <= lowerThreshold) {
+    return 0;
+  }
+
+  if (grossAnnualIncome <= upperThreshold)
+    return ((grossAnnualIncome - lowerThreshold) * lowerRate) / 12;
+
+  return (
+    ((upperThreshold - lowerThreshold) * lowerRate +
+      (grossAnnualIncome - upperThreshold) * upperRate) /
+    12
+  );
+};
+
+export { calculateMonthlyNationalInsurance };
